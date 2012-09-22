85c7d94e1eca4532a7a705fc6814edfb30abcd4e
Matthew <wonderzombie@gmail.com>
2012-09-21 23:06:48 -0700

    Print rooms, exits, prompt, & navigate.
    Rooms are a nested map, with the keys being keywords uniquely
    identifying each room.
    
    The prompt part is a bit ugly, but it mostly works. Need to eliminate
    the extra parens around exits.
    
    Still haven't sorted out how I want to do commands. It shouldn't be in
    mainloop, just so we can test it better. Some handle-cmd function would
    then return either the new state (new room, that is), some way to
    no-op, or some way to quit.

--- src/lookclj/core.clj
+++ src/lookclj/core.clj
@@ -1,31 +1,51 @@
 (ns lookclj.core)
 
-(defn applymove [[x y] d]
-  (cond
-    (= d :north) [x (inc y)]
-    (= d :south) [x (dec y)]
-    (= d :east)  [(inc x) y]
-    (= d :west)  [(dec x) y]
-    :else        [x y]))
+(def rooms 
+  {:red-room
+    { :desc "You're in a red room."
+      :exits { :north :white-room }}
+   :white-room
+    { :desc "You're in a white room."
+      :exits { :south :red-room }}})
 
-(defn command [in]
-  (condp contains? in
-    #{"north" "n"}    :north
-    #{"south" "s"}    :south
-    #{"east"  "e"}    :east
-    #{"west"  "w"}    :west
-    #{"quit" "exit"}  :quit
+(defn nextroom [room dir]
+  (if (nil? dir)
+    room
+    (get-in rooms [room :exits dir])))
+
+(defn lookup-cmd [cmd]
+  (condp contains? cmd
+    #{"north" "n"}        :north
+    #{"south" "s"}        :south
+    #{"east"  "e"}        :east
+    #{"west"  "w"}        :west
+    #{"quit" "exit" "q"}  :quit
+    #{"examine" "look"}   :look
     :else nil))
 
+(defn exits [room]
+  (map name (keys (get-in rooms [room :exits]))))
+
+(defn prompt [room]
+  (println (get-in rooms [room :desc]))
+  (println "Exits:" (print-str (exits room)))
+  (print "> ")
+  (flush))
+
+(defn getcmd []
+  (let [in (read-line)]
+    (lookup-cmd in)))
+
 (defn mainloop []
-  (loop [pos [0 0]]
-    (println "You're at:" pos)
-    (let [in  (read-line)
-          cmd (command in)]
-      (if (= cmd :quit)
-        (do
-          (println "Quitting."))
-        (recur (applymove pos cmd))))))
+  (loop [room :red-room]
+    (prompt room)
+    (let [cmd (getcmd)]
+      (case cmd
+        :quit
+          (do (println "Bye."))
+        :look ; no-op for now.
+          (recur room)
+        (recur (nextroom room cmd))))))
 
 (defn -main []
   (do 
